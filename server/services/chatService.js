1abc38dc21d612008a9b12777818c46b323c71c5
pavelelenov <pavelelenov@gmail.com>
2023-06-20 14:58:25 +0300

    feat(server): finish server

--- server/services/chatService.js
+++ server/services/chatService.js
@@ -1,7 +1,16 @@
 let conversations = [
     {
         name: '1',
-        messages: []
+        messages: [
+            {
+                writer:'Pavel',
+                text: "Hi I am Pavel :)"
+            },
+            {
+                writer: "Plamen",
+                text:`Hi Pavel it's nice to meet you`
+            }
+        ]
     },
     {
         name: '2',
@@ -24,10 +33,21 @@ const getAllChats = () => {
 
 const addMessage = (creator, text, conversationName) => {
     let conversation = conversations.find(c => c.name = conversationName);
-    conversation.messages.push({creator, text});
+    conversation.messages.push({ writer: creator, text });
+}
+
+const getConversationByName = (conversationName) => {
+    const conversation = conversations.find(c => c.name == conversationName);
+
+    if (conversation) {
+        return conversation;
+    }
+
+    throw new Error(`Conversation with this name doesn't exist!`);
 }
 
 module.exports = {
     getAllChats,
-    addMessage
+    addMessage,
+    getConversationByName
 }
