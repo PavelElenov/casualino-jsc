9eb85e8cb107d762f23518b2b93b63b3e8aaf4e8
pavelelenov <pavelelenov@gmail.com>
2023-06-22 11:08:59 +0300

    perf: set up eslint

--- client/index.js
+++ client/index.js
@@ -1,77 +1,73 @@
-const io = require("socket.io-client");
-const rl = require('readline').createInterface(process.stdin, process.stdout);
-const axios = require('axios');
-const httpService = require("./services/httpService");
+const io = require('socket.io-client')
+const rl = require('readline').createInterface(process.stdin, process.stdout)
+const httpService = require('./services/httpService')
 
-let conversationName;
-let token;
-let user;
+let conversationName
+let token
+let user
 
 const socket = io('http://localhost:3000', {
-    transports: ["websocket"],
-});
+  transports: ['websocket']
+})
 
 socket.on('message', data => {
-    if (data.conversation == conversationName) {
-        console.log(`${data.writer}: ${data.text}`);
+  if (data.conversation === conversationName) {
+    console.log(`${data.writer}: ${data.text}`)
+  }
+})
+
+console.log('Please login!')
+authenticateUser()
+
+function authenticateUser () {
+  rl.question('Email: ', (email) => {
+    rl.question('Password:', (password) => {
+      loginUser(email, password)
+    })
+  })
+}
+
+async function loginUser (email, password) {
+  const res = await httpService.post('/login', { email, password })
+  const data = await res.json()
+
+  if (res.status === 200) {
+    token = data.token
+    user = data.user
+    askForConversation()
+  } else {
+    console.log(data)
+    authenticateUser()
+  }
+}
+
+async function getChat () {
+  const res = await httpService.get(`/conversations/${conversationName}`, token)
+  const data = await res.json()
+
+  if (res.status === 200) {
+    const conversationMessages = data.messages
+
+    if (conversationMessages.length > 0) {
+      conversationMessages.map(m => m.writer === user.username ? console.log(`You: ${m.text}`) : console.log(`${m.writer}: ${m.text}`))
     }
-});
-
-console.log('Please login!');
-authenticateUser();
-
-function authenticateUser() {
-    rl.question('Email: ', (email) => {
-        rl.question(`Password:`, (password) => {
-            loginUser(email, password);
-        });
-    });
-
+    readMessages()
+  } else {
+    console.log(data)
+    res.status === 401 ? authenticateUser() : askForConversation()
+  }
 }
 
-async function loginUser(email, password) {
-    const res = await httpService.post("/login", { email, password });
-    const data = await res.json();
-
-    if (res.status == 200) {
-        token = data.token;
-        user = data.user;
-        askForConversation();
-    } else {
-        console.log(data);
-        authenticateUser();
-    }
-
+function askForConversation () {
+  rl.question('In which conversation you want to join? 1, 2, 3, 4: ', (conversation) => {
+    conversationName = conversation
+    getChat()
+  })
 }
 
-async function getChat() {
-    const res = await httpService.get(`/conversations/${conversationName}`, token);
-    const data = await res.json();
-
-    if (res.status == 200) {
-        const conversationMessages = data.messages;
-
-        if (conversationMessages.length > 0) {
-            conversationMessages.map(m => m.writer == user.username ? console.log(`You: ${m.text}`) : console.log(`${m.writer}: ${m.text}`))
-        }
-        readMessages();
-    }else{
-        console.log(data);
-        askForConversation();
-    }
+function readMessages () {
+  rl.on('line', (line) => {
+    socket.emit('message', { writer: user.username, text: line, conversation: conversationName, token })
+    console.log(`You: ${line}`)
+  })
 }
-
-function askForConversation() {
-    rl.question('In which conversation you want to join? 1, 2, 3, 4: ', (conversation) => {
-        conversationName = conversation;
-        getChat();
-    });
-}
-
-function readMessages() {
-    rl.on('line', (line) => {
-        socket.emit('message', { writer: user.username, text: line, conversation: conversationName, token });
-        console.log(`You: ${line}`);
-    });
-}
-
--- server/middlewares/corsMiddleware.ts
+++ server/middlewares/corsMiddleware.ts
@@ -1,20 +1,20 @@
-import { Request, Response, NextFunction } from "express";
-import { verifyToken } from "../utils/createJsonToken";
+import { Request, Response, NextFunction } from 'express';
+import { verifyToken } from '../utils/createJsonToken';
 
 export const addCorsHeaders = () => (req: Request, res: Response, next: NextFunction) => {
-    res.setHeader('Access-Control-Allow-Origin', "*");
+	res.setHeader('Access-Control-Allow-Origin', '*');
 
-    res.setHeader("Access-Control-Allow-Methods", 'GET, POST, PUT, DELETE, OPTIONS, PATCH');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH');
 
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+	res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
-    next();
+	next();
 };
 
 export const addUserToRequest = () => (req: Request, res: Response, next: NextFunction) => {
-    if(req.headers.authorization){
-        const user = verifyToken(req.headers.authorization);
-        req.body['user'] = user;
-    }
-    next();
-}
+	if(req.headers.authorization){
+		const user = verifyToken(req.headers.authorization);
+		req.body['user'] = user;
+	}
+	next();
+};
