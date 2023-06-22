e41efdc2b0b00d983be978ed195b7f0e69b952ef
pavelelenov <pavelelenov@gmail.com>
2023-06-22 10:30:26 +0300

    feat(server): check for authentication in controller
    return response with status 401 with error message if user isn't authenticate

--- server/controllers/chatController.ts
+++ server/controllers/chatController.ts
@@ -1,24 +1,42 @@
 import { IConversation } from "../interfaces/conversation";
 import { getAllChats, getConversationByName } from "../services/chatService";
 import express from 'express';
+import { checkUserAuthentication } from "../utils/checkForUserAuthentication";
 
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    const chats:IConversation[] = getAllChats();
-    res.json(chats);
+    try {
+        const auth: boolean | string = checkUserAuthentication(req);
+
+        if(auth){
+            const chats: IConversation[] = getAllChats();
+            res.json(chats);
+        }else{
+            res.status(401).json(auth);
+        }
+        
+    } catch (error: any) {
+        res.status(401);
+        res.json(error.message);
+    }
+
 });
 
 router.get("/:name", (req, res) => {
-    try{
-        const conversation = getConversationByName(req.params.name);
-        res.status(200).json(conversation);
-    }catch(error: any){
+    try {
+        const auth: string | boolean = checkUserAuthentication(req);
+        if (auth) {
+            const conversation = getConversationByName(req.params.name);
+            res.status(200).json(conversation);
+        } else {
+            res.status(401).json(auth);
+        }
+    } catch (error: any) {
         res.status(404);
         res.json(error.message);
     }
-    
 })
 
 module.exports = router;
