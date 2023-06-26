df3dc676156429f40137221768f82b7bcd459e7c
pavelelenov <pavelelenov@gmail.com>
2023-06-26 14:39:05 +0300

    feat(server): add img and level of conversations

--- server/services/chatService.ts
+++ server/services/chatService.ts
@@ -5,28 +5,28 @@ const conversations: IConversation[] = [
     {
         name: '1',
         messages: [
-            {
-                writer: 'Pavel',
-                text: "Hi I am Pavel :)"
-            },
-            {
-                writer: "Plamen",
-                text: `Hi Pavel it's nice to meet you`
-            }
-        ]
+        ],
+        img: "https://i.pinimg.com/736x/f5/35/1b/f5351b460de396c8dfa2c9937f1f211c.jpg",
+        level: 20
     },
     {
         name: '2',
         messages: [
-        ]
+        ],
+        img: "https://img.playbuzz.com/image/upload/ar_1.5,c_pad,f_jpg,b_auto/q_auto:good,f_auto,fl_lossy,w_480,c_limit,dpr_1/cdn/132af0ca-7be7-4cd8-84e3-9ad41df5c6a7/61791061-eb65-4342-a5f4-af1ae165edef_560_420.jpg",
+        level: 30
     },
     {
         name: '3',
-        messages: []
+        messages: [],
+        img: "https://png.pngtree.com/background/20230519/original/pngtree-three-smiling-faces-in-sunglasses-on-a-dark-background-picture-image_2657621.jpg",
+        level: 40
     },
     {
         name: '4',
-        messages: []
+        messages: [],
+        img: "https://cdn-icons-png.flaticon.com/512/6540/6540753.png",
+        level: 50
     },
 ];
 
@@ -36,7 +36,11 @@ export const getAllChats = (): IConversation[] => {
 
 export const addMessage = (writer:string, text:string, conversationName:string): void => {
     const conversation: IConversation | undefined = conversations.find(c => c.name = conversationName);
-    conversation?.messages.push({ writer, text });
+    const date = new Date();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const totalTime = hours * 60 + minutes;
+    conversation?.messages.push({ writer, text, time: totalTime });
 }
 
 export const getConversationByName = (conversationName: string): IConversation | Error => {
